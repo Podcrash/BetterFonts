4f51072139f3bc8650b2b18061d3eebb82d48183
Furrrlo <ferloisbad@gmail.com>
2021-02-17 21:37:11 +0100

    Quick implementation of the RANDOM renderStyle

--- src/main/java/betterfonts/StringCache.java
+++ src/main/java/betterfonts/StringCache.java
@@ -75,6 +75,15 @@ public class StringCache
     /** Thickness of the strikethrough line (in pixels) */
     private static final int STRIKETHROUGH_THICKNESS = 2;
 
+    /* Characters that are supported by the RANDOM render style. This is pre-sorted to allow being binarySearched */
+    private static final char[] RANDOM_STYLE_CHARS;
+    static
+    {
+        final char[] unsorted = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".toCharArray();
+        Arrays.sort(unsorted); // This is probably already sorted, but just to make 100% sure, I'm gonna manually sort it
+        RANDOM_STYLE_CHARS = unsorted;
+    }
+
     /** Reference to the unicode.FontRenderer class. Needed for creating GlyphVectors and retrieving glyph texture coordinates. */
     private final GlyphCache glyphCache;
 
@@ -117,11 +126,14 @@ public class StringCache
      */
     private final Glyph[][] digitGlyphs = new Glyph[4][];
 
-    private int cachedTexMinFilter, cachedTexMagFilter;
-    private float cachedTexMaxLevel, cachedTexLodBias;
-
     /** True if digitGlyphs[] has been assigned and cacheString() can begin replacing all digits with '0' in the string. */
     private boolean digitGlyphsReady = false;
+
+    /** Random used for the random style in {@link #renderString(String, int, int, int, boolean)} */
+    private final Random fontRandom = new Random();
+
+    private int cachedTexMinFilter, cachedTexMagFilter;
+    private float cachedTexMaxLevel, cachedTexLodBias;
 
     /** If true, then enable GL_BLEND in renderString() so anti-aliasing font glyphs show up properly. */
     private boolean antiAliasEnabled = false;
@@ -434,7 +446,6 @@ public class StringCache
      * @return the total advance (horizontal distance) of this string
      *
      * @todo Add optional NumericShaper to replace ASCII digits with locale specific ones
-     * @todo Add support for the "k" code which randomly replaces letters on each render (used only by splash screen)
      */
     public int renderString(String str, int startX, int startY, int initialColor, boolean shadowFlag)
     {
@@ -531,9 +542,19 @@ public class StringCache
             }
 
             /* Replace character with random one */
-            if((renderStyle & ColorCode.RANDOM) != 0)
+            if((renderStyle & ColorCode.RANDOM) != 0 && Arrays.binarySearch(RANDOM_STYLE_CHARS, c) >= 0)
             {
-                // TODO: random
+                // TODO: probably shouldn't cache calls to getStringWidth() and the subsequent cacheString()
+                int oldCharWidth = getStringWidth(String.valueOf(c));;
+
+                char newC;
+                do {
+                    int charPos = fontRandom.nextInt(RANDOM_STYLE_CHARS.length);
+                    newC = RANDOM_STYLE_CHARS[charPos];
+                }
+                while(oldCharWidth == getStringWidth(String.valueOf(newC)));
+
+                texture = cacheString(String.valueOf(newC)).sortedGlyphs[0].texture;
             }
 
             /*
